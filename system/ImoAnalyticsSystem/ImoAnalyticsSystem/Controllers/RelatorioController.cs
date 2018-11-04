0be944c76a8ac4cd9cacbfe466bec62967b44a9b
Rafael Ueda <rafaueeda@gmal.com>
2018-11-04 13:31:33 -0200

    Finished one of the custom reports

--- system/ImoAnalyticsSystem/ImoAnalyticsSystem/Controllers/RelatorioController.cs
+++ system/ImoAnalyticsSystem/ImoAnalyticsSystem/Controllers/RelatorioController.cs
@@ -558,8 +558,11 @@ namespace ImoAnalyticsSystem.Controllers
             List<string> legenda = new List<string>();
             List<Series> conteudoGrafico = new List<Series>();
             DateTime dataFim = new DateTime(model.DataFim.Year, model.DataFim.Month, model.DataFim.Day);
+            TipoImovelBusiness tipoImovelBusiness = new TipoImovelBusiness();
+            ViewBag.TipoImovelId = new SelectList(tipoImovelBusiness.GetTiposImovel(), "ID", "Tipo", model.TipoImovelId);
+            ViewBag.Imoveis = true;
 
-            while(DateTime.Compare(dataFim, model.DataInicio) >= 0)
+            while (DateTime.Compare(dataFim, model.DataInicio) >= 0)
             {
                 datas.Add(dataFim);
                 dataFim = new DateTime(dataFim.Year, dataFim.Month, dataFim.Day).AddMonths(-1);
@@ -592,19 +595,8 @@ namespace ImoAnalyticsSystem.Controllers
             model.Chart.SetXAxis(new XAxis()
             {
                 Type = AxisTypes.Category,
-                Title = new XAxisTitle() { Text = "Meses", Style = "fontWeight: 'bold', fontSize: '14px'" },
+                Title = new XAxisTitle() { Text = "", Style = "fontWeight: 'bold', fontSize: '14px'" },
                 Categories = legenda.Count() > 1 ? legenda.ToArray() : new string[] { "" }
-            });
-            model.Chart.SetYAxis(new YAxis()
-            {
-                Title = new YAxisTitle()
-                {
-                    Text = "Quantidade de vendas",
-                    Style = "fontWeight: 'bold', fontSize: '14px'"
-                },
-                ShowFirstLabel = true,
-                ShowLastLabel = true,
-                Min = 0
             });
             model.Chart.SetLegend(new Legend
             {
@@ -637,18 +629,112 @@ namespace ImoAnalyticsSystem.Controllers
                         consulta = consulta.Where(c => c.Imovel.QntSuites == model.QntdSuites).ToList();
                     if (model.QntdVagasGaragem != null)
                         consulta = consulta.Where(c => c.Imovel.VagasGaragem == model.QntdVagasGaragem).ToList();
+                    if (model.ValorLocacao != null)
+                        consulta = consulta.Where(c => c.ContratoDeLocacao.Valor == model.ValorLocacao).ToList();
 
-                    foreach(DateTime data in datas)
+                    foreach (DateTime data in datas)
                     {
-                        consulta = consulta.Where(c => c.DataOperacao.Year == data.Year && c.DataOperacao.Month == data.Month).ToList();
-                        numeroVistas.Add(consulta.Count());
+                        var tmp = consulta;
+                        tmp = tmp.Where(c => c.DataOperacao.Year == data.Year && c.DataOperacao.Month == data.Month).ToList();
+                        numeroVistas.Add(tmp.Count());
                     }
 
                     for(int i = numeroVistas.Count() -1; i >= 0 ; i--)
                     {
                         DateTime data = datas.ElementAt(i);
+                        List<Object> locacaoMes = new List<Object>();
+                        locacaoMes.Add(numeroVistas.ElementAt(i));
+                        conteudoGrafico.Add(new Series
+                        {
+                            Name = data.ToString("MMMM yyyy"),
+                            Data = new DotNet.Highcharts.Helpers.Data(locacaoMes.ToArray())
+                        });
+                    }
+
+                    model.Chart.SetYAxis(new YAxis()
+                    {
+                        Title = new YAxisTitle()
+                        {
+                            Text = "Quantidade de locações",
+                            Style = "fontWeight: 'bold', fontSize: '14px'"
+                        },
+                        ShowFirstLabel = true,
+                        ShowLastLabel = true,
+                        Min = 0
+                    });
+                }
+                else if (model.TipoAcao == TipoAcao.Venda)
+                {
+                    List<Venda> consulta = vendaBusiness.GetVendas();
+                    List<Object> numeroVendas = new List<Object>();
+                    if (model.Endereco != null)
+                        consulta = consulta.Where(c => c.Imovel.Endereco.Contains(model.Endereco)).ToList();
+                    if (model.Bairro != null)
+                        consulta = consulta.Where(c => c.Imovel.Bairro.Contains(model.Bairro)).ToList();
+                    if (model.TipoImovelId != null)
+                        consulta = consulta.Where(c => c.Imovel.TipoImovelId == model.TipoImovelId).ToList();
+                    if (model.QntdBanheiros != null)
+                        consulta = consulta.Where(c => c.Imovel.QntBanheiros == model.QntdBanheiros).ToList();
+                    if (model.QntdDormitorios != null)
+                        consulta = consulta.Where(c => c.Imovel.QntDormitorios == model.QntdDormitorios).ToList();
+                    if (model.QntdSuites != null)
+                        consulta = consulta.Where(c => c.Imovel.QntSuites == model.QntdSuites).ToList();
+                    if (model.QntdVagasGaragem != null)
+                        consulta = consulta.Where(c => c.Imovel.VagasGaragem == model.QntdVagasGaragem).ToList();
+                    if (model.ValorVenda != null)
+                        consulta = consulta.Where(c => c.ValorVenda == model.ValorVenda).ToList();
+
+                    foreach (DateTime data in datas)
+                    {
+                        var tmp = consulta;
+                        tmp = tmp.Where(c => c.DataVenda.Year == data.Year && c.DataVenda.Month == data.Month).ToList();
+                        numeroVendas.Add(tmp.Count());
+                    }
+
+                    for (int i = numeroVendas.Count() - 1; i >= 0; i--)
+                    {
+                        DateTime data = datas.ElementAt(i);
+                        List<Object> vendaMes = new List<Object>();
+                        vendaMes.Add(numeroVendas.ElementAt(i));
+                        conteudoGrafico.Add(new Series
+                        {
+                            Name = data.ToString("MMMM yyyy"),
+                            Data = new DotNet.Highcharts.Helpers.Data(vendaMes.ToArray())
+                        });
+                    }
+
+                }
+                else if (model.TipoAcao == TipoAcao.Visita)
+                {
+                    List<Visita> consulta = visitaBusiness.GetVisitas();
+                    List<Object> numeroVisitas = new List<Object>();
+                    if (model.Endereco != null)
+                        consulta = consulta.Where(c => c.Imovel.Endereco.Contains(model.Endereco)).ToList();
+                    if (model.Bairro != null)
+                        consulta = consulta.Where(c => c.Imovel.Bairro.Contains(model.Bairro)).ToList();
+                    if (model.TipoImovelId != null)
+                        consulta = consulta.Where(c => c.Imovel.TipoImovelId == model.TipoImovelId).ToList();
+                    if (model.QntdBanheiros != null)
+                        consulta = consulta.Where(c => c.Imovel.QntBanheiros == model.QntdBanheiros).ToList();
+                    if (model.QntdDormitorios != null)
+                        consulta = consulta.Where(c => c.Imovel.QntDormitorios == model.QntdDormitorios).ToList();
+                    if (model.QntdSuites != null)
+                        consulta = consulta.Where(c => c.Imovel.QntSuites == model.QntdSuites).ToList();
+                    if (model.QntdVagasGaragem != null)
+                        consulta = consulta.Where(c => c.Imovel.VagasGaragem == model.QntdVagasGaragem).ToList();
+
+                    foreach (DateTime data in datas)
+                    {
+                        var tmp = consulta;
+                        tmp = tmp.Where(c => c.Data.Year == data.Year && c.Data.Month == data.Month).ToList();
+                        numeroVisitas.Add(tmp.Count());
+                    }
+
+                    for (int i = numeroVisitas.Count() - 1; i >= 0; i--)
+                    {
+                        DateTime data = datas.ElementAt(i);
                         List<Object> visitaMes = new List<Object>();
-                        visitaMes.Add(numeroVistas.ElementAt(i));
+                        visitaMes.Add(numeroVisitas.ElementAt(i));
                         conteudoGrafico.Add(new Series
                         {
                             Name = data.ToString("MMMM yyyy"),
@@ -656,18 +742,13 @@ namespace ImoAnalyticsSystem.Controllers
                         });
                     }
                 }
-                else if (model.TipoAcao == TipoAcao.Venda)
-                {
-                    List<Venda> consulta = vendaBusiness.GetVendas();
-                }
-                else if (model.TipoAcao == TipoAcao.Visita)
-                {
-                    List<Visita> consulta = visitaBusiness.GetVisitas();
-                }
             }
             else if(model.TipoRelatorio == TipoRelatorio.Estatisticas)
             {
-
+                LocacaoBusiness locacaoBusiness = new LocacaoBusiness();
+                VendaBusiness vendaBusiness = new VendaBusiness();
+                ImovelBusiness imovelBusiness = new ImovelBusiness();
+                VisitaBusiness visitaBusiness = new VisitaBusiness();
             }
             else if (model.TipoRelatorio == TipoRelatorio.Imovel)
             {
